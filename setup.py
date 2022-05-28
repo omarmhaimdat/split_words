eeb9327b1472bdf262fa05025b00c979c725af74
Omar MHAIMDAT <omarmhaimdat@gmail.com>
2022-05-28 19:27:58 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import setuptools
+
+VERSION = "0.1.0"
+DESCRIPTION = "A Python package for splitting text into sentences."
+
+setup(
+    name="split_words",
+    version=VERSION,
+    author="Omar MHAIMDAT",
+    author_email="omarmhaimdat@gmail.com",
+    url="https://github.com/omarmhaimdat/split_words",
+    install_requires=[
+    ],
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    python_requires=">=3.6",
+)
